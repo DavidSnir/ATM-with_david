bc80c94df3996d1b29e3096a953e6c02870e1f00
David Snir <david.snir@gmail.com>
2026-04-03 15:39:28 +0300

    [feature] added style.py

---
+++ styles.py
@@ -0,0 +1,15 @@
+#COLORS
+terminal_green = "#67DF70"
+terminal_red = "#F76161"
+terminal_yellow = "#FABC45"
+text_color = "#E5E5E5"
+dark_bg = "#0D1117"
+less_dark_bg = "#181C22"
+
+#FONTS
+page_title = ("Space Grotesk", 30, "bold")
+page_sub_title = ("Space Grotesk", 24, "light")
+balance = ("Space Grotesk", 54, "light")
+button = ("Space Grotesk", 18, "bold")
+field = ("Space Grotesk", 18, "light")
+detils = ("Space Grotesk", 12, "light")
